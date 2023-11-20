65796fad575e786776a08ba40e89bc8eac908e9b
ismailShaikh12 <Ismail.Shaikh@teknopoint.local>
2023-11-20 16:41:07 +0530

    three project added

--- 05PassGenarator/src/App.jsx
+++ 05PassGenarator/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
 
-    <div className="w-full  h-screen bg-gray-900 flex justify-center align-middle">
-      <h1 className="text-4xl text-center text-white w-1"> hello I am pass genarator</h1>
+    <div className="w-full  h-screen bg-gray-900  justify-center align-middle">
+      <h1 className="text-4xl text-center text-white "> hello I am pass genarator</h1>
       <h2 className="text-4xl text-center text-white"> hello I am pass genarator</h2>
     </div>
 
